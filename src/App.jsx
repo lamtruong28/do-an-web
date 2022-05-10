66dbf2946971247cd7732fcc30ef7ae8ba121fd6
Lequilong <longcode2504@gmail.com>
2022-05-10 23:58:10 +0700

    App-Slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Body from './components/Body'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Slider from './components/Slider'
 
 function App() {
 
   return (
     <div className="app">
       <Header />
+      <Slider />
       <Body />
       <Footer />
     </div>
